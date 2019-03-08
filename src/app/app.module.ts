d400777e1354b682798810ee1e17d7fbe5d32ace
Kevin <menthos984@gmail.com>
2019-03-08 13:07:55 +0800

    online resume

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { CounterComponent } from './counter/counter.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    NavbarComponent,
+    AboutComponent,
+    ServicesComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    CounterComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
