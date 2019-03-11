dcea3975dfa83ee317fd219f9060cb75703ad6d2
Kevin <menthos984@gmail.com>
2019-03-11 11:06:06 +0800

    Fix to enable viewing in heroku

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static('../Website/dist/Website/'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname + '/dist/Website/'));
+    
+res.sendFile(path.join(__dirname,'../Website/dist/Website/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
+console.log("Starting Server");
 app.listen(process.env.PORT || 8080);
